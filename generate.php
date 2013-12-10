f93a7ee441c8e36d39e6bea06366d12a3bf61402
Rienier <rienier@zeresoft.com>
2013-12-10 11:40:36 +0800

    added pdf saving location

--- generate.php
+++ generate.php
@@ -268,11 +268,12 @@
     	</div>
     </div>
 </page>";
-
+    $filename = 'forms/'.date('Y-m-d_H:i:s_A').".pdf";
     require_once(dirname(__FILE__).'/html2pdf/html2pdf.class.php');
     $html2pdf = new HTML2PDF('P','Letter','fr');
     $html2pdf->WriteHTML($content);
-    $html2pdf->Output('exemple.pdf');
+    $html2pdf->Output('preview.pdf');
+    $html2pdf->Output($filename, 'F');
 
     //echo $content;
 ?>
